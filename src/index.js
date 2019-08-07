244e0e05472b438ece199afcb84bb5677e30f612
j_bleach <513755565@qq.com>
2019-08-07 13:55:34 +0800

    webpack 配置

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by j_bleach on 2018/10/23 0023.
+ */
+import map2img from "./map2img";
+
+export default map2img;
---
+++ src/map2img.js
@@ -0,0 +1,15 @@
+/**
+ * Created by j_bleach on 2018/10/23 0023.
+ */
+/*eslint-disable*/
+
+class Map2img {
+    constructor() {
+      console.log(1111)
+    }
+
+    init() {
+    }
+}
+
+export default new Map2img();
